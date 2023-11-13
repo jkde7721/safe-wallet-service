bdaf9047dc4ff0eea055ddd55b8b407b6315e65e
Daeun Kim <jkde7721@naver.com>
2023-11-13 18:18:49 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/wanted/safewallet/SafeWalletApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.safewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/safewallet/SafeWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.safewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
