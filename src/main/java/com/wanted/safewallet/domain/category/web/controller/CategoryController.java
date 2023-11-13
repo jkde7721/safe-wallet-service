d73475e91f0be784f9f8c3f6a482eb42ac271349
Daeun Kim <jkde7721@naver.com>
2023-11-14 02:19:22 +0900

    feat: 카테고리 목록 조회 컨트롤러 구현
    [SW-2]

---
+++ src/main/java/com/wanted/safewallet/domain/category/web/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.wanted.safewallet.domain.category.web.controller;
+
+import com.wanted.safewallet.domain.category.business.service.CategoryService;
+import com.wanted.safewallet.domain.category.web.dto.response.CategoryListResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/categories")
+@RestController
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<CategoryListResponseDto> getCategoryList() {
+        CategoryListResponseDto dto = categoryService.getCategoryList();
+        return ResponseEntity.ok(dto);
+    }
+}
