d54aa698b6054df1f32788c1a9d6d11e067c555f
Daeun Kim <jkde7721@naver.com>
2023-11-14 14:34:45 +0900

    test: 카테고리 유효성 검증 테스트
    [SW-12]

--- src/test/java/com/wanted/safewallet/domain/category/business/service/CategoryServiceTest.java
+++ src/test/java/com/wanted/safewallet/domain/category/business/service/CategoryServiceTest.java
@@ -1,16 +1,19 @@
 package com.wanted.safewallet.domain.category.business.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
+import com.wanted.safewallet.domain.category.business.dto.request.CategoryValidRequestDto;
 import com.wanted.safewallet.domain.category.business.mapper.CategoryMapper;
 import com.wanted.safewallet.domain.category.persistence.entity.Category;
 import com.wanted.safewallet.domain.category.persistence.entity.CategoryType;
 import com.wanted.safewallet.domain.category.persistence.repository.CategoryRepository;
 import com.wanted.safewallet.domain.category.web.dto.response.CategoryListResponseDto;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -52,5 +55,24 @@ class CategoryServiceTest {
         assertThat(responseDto.getCategoryList())
             .extracting("type")
             .contains(CategoryType.FOOD, CategoryType.TRAFFIC);
+    }
+
+    @DisplayName("카테고리 유효성 검증 테스트 : 실패 - 존재하지 않는 카테고리")
+    @Test
+    void validateCategoryFail() {
+        //given
+        Map<Long, Category> categoryMap = Map.of(
+            1L, Category.builder().id(1L).type(CategoryType.FOOD).build(),
+            2L, Category.builder().id(2L).type(CategoryType.TRAFFIC).build());
+        given(categoryRepository.findAllMap()).willReturn(categoryMap);
+
+        //when
+        List<CategoryValidRequestDto> categoryValidRequestDtoList = List.of(
+            new CategoryValidRequestDto(1L, CategoryType.TRAFFIC));
+
+        //then
+        assertThatThrownBy(() -> categoryService.validateCategory(categoryValidRequestDtoList))
+            .isInstanceOf(RuntimeException.class);
+        then(categoryRepository).should(times(1)).findAllMap();
     }
 }
