7e4bdc53767bcd0844d5f3d5863708c324732183
Daeun Kim <jkde7721@naver.com>
2023-11-14 13:05:21 +0900

    feat: 현재 사용자의 같은 날짜, 같은 카테고리의 기존 예산 설정 내역 존재하는지 확인하는 레포지토리 구현
    - 만약 존재한다면, 설정 불가
    
    [SW-12]

--- src/main/java/com/wanted/safewallet/domain/budget/persistence/repository/BudgetRepository.java
+++ src/main/java/com/wanted/safewallet/domain/budget/persistence/repository/BudgetRepository.java
@@ -3,6 +3,6 @@ package com.wanted.safewallet.domain.budget.persistence.repository;
 import com.wanted.safewallet.domain.budget.persistence.entity.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BudgetRepository extends JpaRepository<Budget, Long> {
+public interface BudgetRepository extends JpaRepository<Budget, Long>, BudgetRepositoryCustom {
 
 }
