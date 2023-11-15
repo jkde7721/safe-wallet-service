a4216443f097d5ab4be483bd3f06067af6d35feb
Daeun Kim <jkde7721@naver.com>
2023-11-15 20:21:38 +0900

    update: 삭제되지 않은 지출 내역만 조회하는 findById 메소드 오버라이딩
    [SW-18]

--- src/main/java/com/wanted/safewallet/domain/expenditure/persistence/repository/ExpenditureRepository.java
+++ src/main/java/com/wanted/safewallet/domain/expenditure/persistence/repository/ExpenditureRepository.java
@@ -1,8 +1,13 @@
 package com.wanted.safewallet.domain.expenditure.persistence.repository;
 
 import com.wanted.safewallet.domain.expenditure.persistence.entity.Expenditure;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> {
 
+    @Query("select e from Expenditure e where e.id = :expenditureId and e.deleted = false")
+    Optional<Expenditure> findById(@Param("expenditureId") Long expenditureId);
 }
