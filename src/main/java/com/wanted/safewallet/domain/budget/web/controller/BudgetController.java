8ca2c3f270e33cb5e2d061deb84ad09c2e473d53
Daeun Kim <jkde7721@naver.com>
2023-11-15 11:30:54 +0900

    feat: 공통 응답 aop 커스텀 어노테이션 컨트롤러에 적용
    - 커스텀 @CommonResponseContent 어노테이션 사용
    
    [SW-21]

--- src/main/java/com/wanted/safewallet/domain/budget/web/controller/BudgetController.java
+++ src/main/java/com/wanted/safewallet/domain/budget/web/controller/BudgetController.java
@@ -3,16 +3,17 @@ package com.wanted.safewallet.domain.budget.web.controller;
 import com.wanted.safewallet.domain.budget.business.service.BudgetService;
 import com.wanted.safewallet.domain.budget.web.dto.request.BudgetSetUpRequestDto;
 import com.wanted.safewallet.domain.budget.web.dto.response.BudgetSetUpResponseDto;
+import com.wanted.safewallet.global.dto.response.aop.CommonResponseContent;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CommonResponseContent
 @RequiredArgsConstructor
 @RequestMapping("/api/budgets")
 @RestController
@@ -23,9 +24,9 @@ public class BudgetController {
     @Value("${temporary.userId}")
     private String userId; //security 구현 전 임시 사용자 ID
 
+    @CommonResponseContent(status = HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity<BudgetSetUpResponseDto> setUpBudget(@RequestBody @Valid BudgetSetUpRequestDto requestDto) {
-        BudgetSetUpResponseDto responseDto = budgetService.setUpBudget(userId, requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    public BudgetSetUpResponseDto setUpBudget(@RequestBody @Valid BudgetSetUpRequestDto requestDto) {
+        return budgetService.setUpBudget(userId, requestDto);
     }
 }
--- src/main/java/com/wanted/safewallet/domain/category/web/controller/CategoryController.java
+++ src/main/java/com/wanted/safewallet/domain/category/web/controller/CategoryController.java
@@ -2,12 +2,13 @@ package com.wanted.safewallet.domain.category.web.controller;
 
 import com.wanted.safewallet.domain.category.business.service.CategoryService;
 import com.wanted.safewallet.domain.category.web.dto.response.CategoryListResponseDto;
+import com.wanted.safewallet.global.dto.response.aop.CommonResponseContent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CommonResponseContent
 @RequiredArgsConstructor
 @RequestMapping("/api/categories")
 @RestController
@@ -16,8 +17,7 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<CategoryListResponseDto> getCategoryList() {
-        CategoryListResponseDto responseDto = categoryService.getCategoryList();
-        return ResponseEntity.ok(responseDto);
+    public CategoryListResponseDto getCategoryList() {
+        return categoryService.getCategoryList();
     }
 }
