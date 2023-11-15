438e3aba9088bfed3b84949cfd837224fd6ef8b7
Daeun Kim <jkde7721@naver.com>
2023-11-15 13:48:17 +0900

    feat: 지출 내역 생성 서비스 구현
    [SW-3]

---
+++ src/main/java/com/wanted/safewallet/domain/expenditure/business/service/ExpenditureService.java
@@ -0,0 +1,37 @@
+package com.wanted.safewallet.domain.expenditure.business.service;
+
+import com.wanted.safewallet.domain.category.business.dto.request.CategoryValidRequestDto;
+import com.wanted.safewallet.domain.category.business.service.CategoryService;
+import com.wanted.safewallet.domain.expenditure.business.mapper.ExpenditureMapper;
+import com.wanted.safewallet.domain.expenditure.persistence.entity.Expenditure;
+import com.wanted.safewallet.domain.expenditure.persistence.repository.ExpenditureRepository;
+import com.wanted.safewallet.domain.expenditure.web.dto.request.ExpenditureCreateRequestDto;
+import com.wanted.safewallet.domain.expenditure.web.dto.response.ExpenditureCreateResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class ExpenditureService {
+
+    private final ExpenditureMapper expenditureMapper;
+    private final CategoryService categoryService;
+    private final ExpenditureRepository expenditureRepository;
+
+    @Transactional
+    public ExpenditureCreateResponseDto createExpenditure(String userId,
+        ExpenditureCreateRequestDto requestDto) {
+        validateRequest(requestDto);
+        Expenditure expenditure = expenditureMapper.toEntity(userId, requestDto);
+        expenditureRepository.save(expenditure);
+        return expenditureMapper.toDto(expenditure);
+    }
+
+    private void validateRequest(ExpenditureCreateRequestDto requestDto) {
+        CategoryValidRequestDto categoryValidDto = new CategoryValidRequestDto(
+            requestDto.getCategoryId(), requestDto.getType());
+        categoryService.validateCategory(categoryValidDto);
+    }
+}
