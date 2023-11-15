156697a0a19b7c53fcc54c0795ff4d038fc2252d
Daeun Kim <jkde7721@naver.com>
2023-11-15 14:30:24 +0900

    test: 지출 내역 생성 컨트롤러 테스트
    [SW-3]

--- src/main/java/com/wanted/safewallet/domain/expenditure/web/controller/ExpenditureController.java
+++ src/main/java/com/wanted/safewallet/domain/expenditure/web/controller/ExpenditureController.java
@@ -7,6 +7,7 @@ import com.wanted.safewallet.global.dto.response.aop.CommonResponseContent;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ public class ExpenditureController {
     @Value("${temporary.userId}")
     private String userId; //security 구현 전 임시 사용자 ID
 
+    @CommonResponseContent(status = HttpStatus.CREATED)
     @PostMapping
     public ExpenditureCreateResponseDto createExpenditure(@RequestBody @Valid ExpenditureCreateRequestDto requestDto) {
         return expenditureService.createExpenditure(userId, requestDto);
---
+++ src/test/java/com/wanted/safewallet/domain/expenditure/web/controller/ExpenditureControllerTest.java
@@ -0,0 +1,77 @@
+package com.wanted.safewallet.domain.expenditure.web.controller;
+
+import static com.wanted.safewallet.utils.JsonUtils.asJsonString;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.wanted.safewallet.domain.category.persistence.entity.CategoryType;
+import com.wanted.safewallet.domain.expenditure.business.service.ExpenditureService;
+import com.wanted.safewallet.domain.expenditure.web.dto.request.ExpenditureCreateRequestDto;
+import com.wanted.safewallet.domain.expenditure.web.dto.response.ExpenditureCreateResponseDto;
+import java.time.LocalDate;
+import org.hamcrest.core.AllOf;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ExpenditureController.class)
+class ExpenditureControllerTest {
+
+    @MockBean
+    ExpenditureService expenditureService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @DisplayName("지출 내역 생성 컨트롤러 테스트 : 성공")
+    @Test
+    void createExpenditure() throws Exception {
+        //given
+        ExpenditureCreateResponseDto responseDto = new ExpenditureCreateResponseDto(1L);
+        given(expenditureService.createExpenditure(anyString(), any(ExpenditureCreateRequestDto.class)))
+            .willReturn(responseDto);
+
+        //when, then
+        ExpenditureCreateRequestDto requestDto = new ExpenditureCreateRequestDto(
+            LocalDate.now(), 10000L, 1L, CategoryType.FOOD, "");
+        mockMvc.perform(post("/api/expenditures")
+                .content(asJsonString(requestDto))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.data.expenditureId").exists())
+            .andDo(print());
+        then(expenditureService).should(times(1)).createExpenditure(anyString(), any(ExpenditureCreateRequestDto.class));
+    }
+
+    @DisplayName("지출 내역 생성 컨트롤러 테스트 : 실패")
+    @Test
+    void createExpenditure_validation_fail() throws Exception {
+        //given
+        ExpenditureCreateRequestDto requestDto = new ExpenditureCreateRequestDto(
+            null, -1L, 1L, CategoryType.FOOD, "");
+
+        //when, then
+        mockMvc.perform(post("/api/expenditures")
+                .content(asJsonString(requestDto))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message", AllOf.allOf(
+                containsString("expenditureDate"), containsString("amount"))))
+            .andDo(print());
+        then(expenditureService).should(times(0)).createExpenditure(anyString(), any(ExpenditureCreateRequestDto.class));
+    }
+}
