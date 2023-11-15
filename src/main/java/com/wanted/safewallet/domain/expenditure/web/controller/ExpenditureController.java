92d9239f0608be424be208311a6a31ef28a09f43
Daeun Kim <jkde7721@naver.com>
2023-11-15 16:49:05 +0900

    feat: 지출 내역 수정 컨트롤러 구현
    [SW-15]

--- src/main/java/com/wanted/safewallet/domain/expenditure/web/controller/ExpenditureController.java
+++ src/main/java/com/wanted/safewallet/domain/expenditure/web/controller/ExpenditureController.java
@@ -2,13 +2,16 @@ package com.wanted.safewallet.domain.expenditure.web.controller;
 
 import com.wanted.safewallet.domain.expenditure.business.service.ExpenditureService;
 import com.wanted.safewallet.domain.expenditure.web.dto.request.ExpenditureCreateRequestDto;
+import com.wanted.safewallet.domain.expenditure.web.dto.request.ExpenditureUpdateRequestDto;
 import com.wanted.safewallet.domain.expenditure.web.dto.response.ExpenditureCreateResponseDto;
 import com.wanted.safewallet.global.dto.response.aop.CommonResponseContent;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,11 @@ public class ExpenditureController {
     @PostMapping
     public ExpenditureCreateResponseDto createExpenditure(@RequestBody @Valid ExpenditureCreateRequestDto requestDto) {
         return expenditureService.createExpenditure(userId, requestDto);
+    }
+
+    @PutMapping("/{expenditureId}")
+    public void updateExpenditure(@PathVariable Long expenditureId,
+        @RequestBody @Valid ExpenditureUpdateRequestDto requestDto) {
+        expenditureService.updateExpenditure(userId, expenditureId, requestDto);
     }
 }
---
+++ src/main/java/com/wanted/safewallet/domain/expenditure/web/dto/request/ExpenditureUpdateRequestDto.java
@@ -0,0 +1,32 @@
+package com.wanted.safewallet.domain.expenditure.web.dto.request;
+
+import com.wanted.safewallet.domain.category.persistence.entity.CategoryType;
+import com.wanted.safewallet.global.dto.request.format.CustomLocalDateFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpenditureUpdateRequestDto {
+
+    @CustomLocalDateFormat
+    @NotNull
+    private LocalDate expenditureDate;
+
+    @Min(0)
+    @NotNull
+    private Long amount;
+
+    @NotNull
+    private Long categoryId;
+
+    @NotNull
+    private CategoryType type;
+
+    private String note = "";
+}
