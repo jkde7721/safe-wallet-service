f6cb2af806a35632da03bafaeff036b4cf572fbf
Daeun Kim <jkde7721@naver.com>
2023-11-15 13:04:58 +0900

    feat: 요청 dto 내 LocalDate 타입 필드에 대한 deserializer 및 어노테이션 구현
    [SW-3]

---
+++ src/main/java/com/wanted/safewallet/global/dto/request/format/CustomLocalDateDeserializer.java
@@ -0,0 +1,42 @@
+package com.wanted.safewallet.global.dto.request.format;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+    private static final String HYPHEN_PATTERN = "yyyy-MM-dd";
+    private static final String SLASH_PATTERN = "yyyy/MM/dd";
+    private static final String DOT_PATTERN = "yyyy.MM.dd";
+    private static final String HYPHEN_PATTERN_NON_ZERO = "yyyy-M-d";
+    private static final String SLASH_PATTERN_NON_ZERO = "yyyy/M/d";
+    private static final String DOT_PATTERN_NON_ZERO = "yyyy.M.d";
+    private static final List<DateTimeFormatter> formatters = List.of(
+        DateTimeFormatter.ofPattern(HYPHEN_PATTERN),
+        DateTimeFormatter.ofPattern(SLASH_PATTERN),
+        DateTimeFormatter.ofPattern(DOT_PATTERN),
+        DateTimeFormatter.ofPattern(HYPHEN_PATTERN_NON_ZERO),
+        DateTimeFormatter.ofPattern(SLASH_PATTERN_NON_ZERO),
+        DateTimeFormatter.ofPattern(DOT_PATTERN_NON_ZERO));
+
+    @Override
+    public LocalDate deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+        for (DateTimeFormatter formatter : formatters) {
+            try {
+                LocalDateDeserializer deserializer = new LocalDateDeserializer(formatter);
+                return deserializer.deserialize(parser, context);
+            } catch (Exception e) {
+            }
+        }
+        throw new JsonParseException(parser, "Unable to parse date: [" + parser.getValueAsString()
+            + "]. Supported formats: " + List.of(HYPHEN_PATTERN, SLASH_PATTERN, DOT_PATTERN,
+            HYPHEN_PATTERN_NON_ZERO, SLASH_PATTERN_NON_ZERO, DOT_PATTERN_NON_ZERO));
+    }
+}
