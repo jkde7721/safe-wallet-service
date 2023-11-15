d975fcdcdcdf332b35f713ea49b5d09215d21a18
Daeun Kim <jkde7721@naver.com>
2023-11-15 11:54:34 +0900

    update: 예외 처리에 대한 응답 dto CommonResponse로 수정
    [SW-21]

--- src/main/java/com/wanted/safewallet/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wanted/safewallet/global/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.wanted.safewallet.global.exception;
 
-import static java.util.stream.Collectors.toMap;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-import java.util.Map;
+import com.wanted.safewallet.global.dto.response.CommonResponse;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,17 +16,21 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+    public ResponseEntity<CommonResponse<Void>> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
         return ResponseEntity.status(errorCode.getHttpStatus())
-            .body(new ErrorResponse(errorCode));
+            .body(new CommonResponse<>(errorCode.getHttpStatus().value(), errorCode.name(),
+                errorCode.getMessage(), null));
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
         MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        Map<String, String> errors = e.getBindingResult().getFieldErrors().stream()
-            .collect(toMap(FieldError::getField, FieldError::getDefaultMessage));
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        StringBuilder sb = new StringBuilder();
+        e.getBindingResult().getFieldErrors().forEach(f ->
+            sb.append("[").append(f.getField()).append(": ").append(f.getDefaultMessage()).append("] "));
+        return ResponseEntity.status(BAD_REQUEST)
+            .body(new CommonResponse<>(BAD_REQUEST.value(), BAD_REQUEST.name(),
+                sb.toString(), null));
     }
 }
