c84121dffd3b0ad8a887ffbdfba2c3861de4a6bd
Daeun Kim <jkde7721@naver.com>
2023-11-19 01:33:32 +0900

    feat: 동일한 계정명의 유저 존재 여부 조회하는 레포지토리 구현
    [SW-1]

--- src/main/java/com/wanted/safewallet/domain/user/persistence/repository/UserRepository.java
+++ src/main/java/com/wanted/safewallet/domain/user/persistence/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.wanted.safewallet.domain.user.persistence.repository;
 
 import com.wanted.safewallet.domain.user.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
+    @Query("SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END FROM User u WHERE u.username = :username")
+    boolean existsByUsername(@Param("username") String username);
 }
