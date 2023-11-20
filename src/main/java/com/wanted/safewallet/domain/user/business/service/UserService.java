fac93f7cfc354decb22715a01cf52df2d505b9ef
Daeun Kim <jkde7721@naver.com>
2023-11-20 12:51:18 +0900

    feat: 유저 회원가입 서비스 구현
    - 비밀번호는 PasswordEncoder를 이용해 단방향 인코딩 처리하여 저장
    
    [SW-1]

--- src/main/java/com/wanted/safewallet/domain/user/business/service/UserService.java
+++ src/main/java/com/wanted/safewallet/domain/user/business/service/UserService.java
@@ -1,9 +1,15 @@
 package com.wanted.safewallet.domain.user.business.service;
 
+import static com.wanted.safewallet.global.exception.ErrorCode.ALREADY_EXISTS_USERNAME;
+
 import com.wanted.safewallet.domain.user.business.mapper.UserMapper;
+import com.wanted.safewallet.domain.user.persistence.entity.User;
 import com.wanted.safewallet.domain.user.persistence.repository.UserRepository;
+import com.wanted.safewallet.domain.user.web.dto.request.UserJoinRequestDto;
 import com.wanted.safewallet.domain.user.web.dto.response.UsernameCheckResponseDto;
+import com.wanted.safewallet.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +20,25 @@ public class UserService {
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UsernameCheckResponseDto isDuplicatedUsername(String username) {
         boolean isDuplicatedUsername = userRepository.existsByUsername(username);
         return userMapper.toDto(isDuplicatedUsername);
+    }
+
+    @Transactional
+    public void joinUser(UserJoinRequestDto requestDto) {
+        checkForUsername(requestDto.getUsername());
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        User user = User.builder().username(username).password(password).build();
+        userRepository.save(user);
+    }
+
+    public void checkForUsername(String username) {
+        if (userRepository.existsByUsername(username)) {
+            throw new BusinessException(ALREADY_EXISTS_USERNAME);
+        }
     }
 }
