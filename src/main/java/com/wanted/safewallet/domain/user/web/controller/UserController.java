df74778a120eca983cf38a6a01cbff1e4523a776
Daeun Kim <jkde7721@naver.com>
2023-11-20 12:55:06 +0900

    feat: 유저 회원가입 컨트롤러 구현
    [SW-1]

--- src/main/java/com/wanted/safewallet/domain/user/web/controller/UserController.java
+++ src/main/java/com/wanted/safewallet/domain/user/web/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.wanted.safewallet.domain.user.web.controller;
 
 import com.wanted.safewallet.domain.user.business.service.UserService;
+import com.wanted.safewallet.domain.user.web.dto.request.UserJoinRequestDto;
 import com.wanted.safewallet.domain.user.web.dto.response.UsernameCheckResponseDto;
 import com.wanted.safewallet.global.dto.response.aop.CommonResponseContent;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +25,11 @@ public class UserController {
     @GetMapping("/{username}")
     public UsernameCheckResponseDto checkForUsername(@PathVariable String username) {
         return userService.isDuplicatedUsername(username);
+    }
+
+    @CommonResponseContent(status = HttpStatus.CREATED)
+    @PostMapping
+    public void joinUser(@RequestBody @Valid UserJoinRequestDto requestDto) {
+        userService.joinUser(requestDto);
     }
 }
