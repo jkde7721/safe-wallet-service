1183fa7265b4fdb2d7c4c64732bdf1fa77cc749e
Daeun Kim <jkde7721@naver.com>
2023-11-22 01:40:47 +0900

    fix: CustomUserDetailsService에서 유저 조회 실패 시, AuthenticationException 예외 던지도록 수정
    - 기존 BusinessException 예외는 AuthenticationFailureHandler가 처리하지 못하기 때문
    
    [SW-11]

--- src/main/java/com/wanted/safewallet/domain/auth/business/service/CustomUserDetailsService.java
+++ src/main/java/com/wanted/safewallet/domain/auth/business/service/CustomUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.wanted.safewallet.domain.auth.business.service;
 
 import com.wanted.safewallet.domain.auth.business.dto.response.CustomUserDetails;
-import com.wanted.safewallet.domain.user.business.service.UserService;
 import com.wanted.safewallet.domain.user.persistence.entity.User;
+import com.wanted.safewallet.domain.user.persistence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,11 +13,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.getUserByUsername(username);
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("잘못된 계정명입니다."));
         return new CustomUserDetails(user);
     }
 }
