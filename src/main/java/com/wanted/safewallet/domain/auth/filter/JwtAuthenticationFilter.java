0337f65119fa02c408193d0bf16b4a75cd8552fa
Daeun Kim <jkde7721@naver.com>
2023-11-22 02:05:37 +0900

    feat: JWT 인증 및 인가 처리 필터 구현
    - 인증이 필요한 API에 대해 JWT로부터 Authentication 객체를 생성
    - SecurityContext에 해당 Authentication 객체를 저장
    
    [SW-11]

---
+++ src/main/java/com/wanted/safewallet/domain/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.wanted.safewallet.domain.auth.filter;
+
+import com.wanted.safewallet.domain.auth.utils.HeaderUtils;
+import com.wanted.safewallet.domain.auth.utils.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtutils;
+    private final HeaderUtils headerUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        String token = headerUtils.getToken(request);
+        if (StringUtils.hasText(token) && jwtutils.validateToken(token)) {
+            String userId = jwtutils.getUserId(token);
+            List<GrantedAuthority> authorities = jwtutils.getAuthorities(token);
+
+            UsernamePasswordAuthenticationToken authentication = UsernamePasswordAuthenticationToken
+                .authenticated(userId, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
