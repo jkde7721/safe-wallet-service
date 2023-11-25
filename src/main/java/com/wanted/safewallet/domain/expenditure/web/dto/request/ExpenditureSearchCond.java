be0775c1c49f4d8b6b3addd5babae73fbf568d5f
Daeun Kim <jkde7721@naver.com>
2023-11-25 22:26:59 +0900

    feat: 지출 내역 목록 조회 시 검색 조건 요청 dto 구현
    - 기본 startDate: 현재로부터 1달 전
    - 기본 endDate: 현재 날짜
    - 기본 categories: 빈 리스트(=모든 카테고리)
    - 기본 minAmount: 0원
    - 기본 maxAmount: 100만원
    
    [SW-17]

---
+++ src/main/java/com/wanted/safewallet/domain/expenditure/web/dto/request/ExpenditureSearchCond.java
@@ -0,0 +1,30 @@
+package com.wanted.safewallet.domain.expenditure.web.dto.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpenditureSearchCond {
+
+    //TODO: 조회 날짜 기간은 최대 1년
+    private LocalDate startDate = LocalDate.now().minusMonths(1);
+
+    private LocalDate endDate = LocalDate.now();
+
+    private List<Long> categories = List.of();
+
+    @Min(0)
+    private Long minAmount = 0L;
+
+    @Max(100000000)
+    private Long maxAmount = 1000000L;
+}
