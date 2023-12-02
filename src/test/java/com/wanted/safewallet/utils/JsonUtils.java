418855068a1fbb2bbfcfe29de8310de095c5d437
Daeun Kim <jkde7721@naver.com>
2023-12-02 19:50:01 +0900

    test: 테스트 시 요청 dto 객체 직렬화하기 위해 사용되는 ObjectMapper 관련 Utils 수정
    ex. YearMonth.of(2023, 12) 객체가 "[2023,12]"로 직렬화되는 문제 해결
    → .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) 설정 추가하여 "2023-12"로 직렬화되도록 함
    
    [SW-23]

--- src/test/java/com/wanted/safewallet/utils/JsonUtils.java
+++ src/test/java/com/wanted/safewallet/utils/JsonUtils.java
@@ -2,15 +2,22 @@ package com.wanted.safewallet.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtils {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
 
     private JsonUtils() {}
 
     public static String asJsonString(Object value) throws JsonProcessingException {
-        return objectMapper.registerModule(new JavaTimeModule()).writeValueAsString(value);
+        return objectMapper.writeValueAsString(value);
     }
 }
