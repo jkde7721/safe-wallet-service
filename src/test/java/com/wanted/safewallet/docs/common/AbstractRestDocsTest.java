125ab1bb2e017af636721b6684fa7ba7b9538b2a
Daeun Kim <jkde7721@naver.com>
2023-12-03 19:32:25 +0900

    docs: WebMvcTest에 Spring Security Filter 적용한 새로운 MockMvc 필드 추가
    [SW-23]

--- src/test/java/com/wanted/safewallet/docs/common/AbstractRestDocsTest.java
+++ src/test/java/com/wanted/safewallet/docs/common/AbstractRestDocsTest.java
@@ -4,17 +4,21 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import com.wanted.safewallet.docs.config.RestDocsConfig;
+import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.security.config.BeanIds;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Import(RestDocsConfig.class)
 @ExtendWith(RestDocumentationExtension.class)
@@ -26,14 +30,27 @@ public abstract class AbstractRestDocsTest {
     @Autowired
     protected MockMvc restDocsMockMvc;
 
+    @Autowired
+    protected MockMvc authRestDocsMockMvc;
+
     @BeforeEach
     void init(final WebApplicationContext context,
-        final RestDocumentationContextProvider provider) {
+        final RestDocumentationContextProvider provider) throws ServletException {
         this.restDocsMockMvc = MockMvcBuilders.webAppContextSetup(context)
             .apply(documentationConfiguration(provider))
             .alwaysDo(print())
             .alwaysDo(restDocs)
             .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .build();
+
+        //Auth 도메인 MockMvc 테스트에 Spring Security Filter 적용
+        DelegatingFilterProxy delegatingFilterProxy = new DelegatingFilterProxy();
+        delegatingFilterProxy.init(new MockFilterConfig(context.getServletContext(), BeanIds.SPRING_SECURITY_FILTER_CHAIN));
+        this.authRestDocsMockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(documentationConfiguration(provider))
+            .alwaysDo(print())
+            .alwaysDo(restDocs)
+            .addFilters(new CharacterEncodingFilter("UTF-8", true), delegatingFilterProxy)
             .build();
     }
 }
