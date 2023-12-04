9ce8e028fcdcf00edb68b5a348e0a2735fe7a0c8
Daeun Kim <jkde7721@naver.com>
2023-12-04 22:54:04 +0900

    feat: 특정 예산 내역 수정 서비스 구현
    - 수정하고자 하는 카테고리, 년월의 기존 예산 내역이 있다면 특정 예산 내역 삭제 후 기존 예산 내역에 금액 추가
    - 없으면 그냥 특정 예산 내역 정보 업데이트
    
    [SW-13]

--- src/main/java/com/wanted/safewallet/domain/budget/persistence/entity/Budget.java
+++ src/main/java/com/wanted/safewallet/domain/budget/persistence/entity/Budget.java
@@ -2,6 +2,7 @@ package com.wanted.safewallet.domain.budget.persistence.entity;
 
 import com.wanted.safewallet.domain.budget.persistence.converter.YearMonthAttributeConverter;
 import com.wanted.safewallet.domain.category.persistence.entity.Category;
+import com.wanted.safewallet.domain.category.persistence.entity.CategoryType;
 import com.wanted.safewallet.domain.user.persistence.entity.User;
 import com.wanted.safewallet.global.audit.BaseTime;
 import jakarta.persistence.Column;
@@ -46,4 +47,14 @@ public class Budget extends BaseTime {
     @Convert(converter = YearMonthAttributeConverter.class)
     @Column(nullable = false)
     private YearMonth budgetYearMonth;
+
+    public void update(Long categoryId, CategoryType type, Long amount, YearMonth budgetYearMonth) {
+        this.category = Category.builder().id(categoryId).type(type).build();
+        this.amount = amount;
+        this.budgetYearMonth = budgetYearMonth;
+    }
+
+    public void addAmount(Long amount) {
+        this.amount += amount;
+    }
 }
