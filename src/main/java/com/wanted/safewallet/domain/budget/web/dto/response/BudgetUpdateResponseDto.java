632364bfe0827bf7af77c602fdaab0206253a5e9
Daeun Kim <jkde7721@naver.com>
2023-12-04 22:55:55 +0900

    feat: 특정 예산 내역 수정 API 관련 mapper, dto 정의
    [SW-13]

---
+++ src/main/java/com/wanted/safewallet/domain/budget/web/dto/response/BudgetUpdateResponseDto.java
@@ -0,0 +1,23 @@
+package com.wanted.safewallet.domain.budget.web.dto.response;
+
+import com.wanted.safewallet.domain.category.persistence.entity.CategoryType;
+import java.time.YearMonth;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class BudgetUpdateResponseDto {
+
+    private Long budgetId;
+
+    private YearMonth budgetYearMonth;
+
+    private Long categoryId;
+
+    private CategoryType type;
+
+    private Long amount;
+}
