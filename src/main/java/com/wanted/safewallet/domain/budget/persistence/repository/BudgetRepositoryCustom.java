f260df87f9ea381e73c82b838ac4cf719465cc8c
Daeun Kim <jkde7721@naver.com>
2023-12-05 21:14:16 +0900

    feat: 월별 예산 내역에서 카테고리별 예산 총액 계산하는 레포지토리 메소드 구현
    - 현재 로그인한 유저의 예산 내역 또는 다른 유저들의 예산 내역
    
    [SW-14]

--- src/main/java/com/wanted/safewallet/domain/budget/persistence/repository/BudgetRepositoryCustom.java
+++ src/main/java/com/wanted/safewallet/domain/budget/persistence/repository/BudgetRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.wanted.safewallet.domain.budget.persistence.repository;
 
+import com.wanted.safewallet.domain.budget.persistence.dto.response.TotalAmountByCategoryResponseDto;
 import java.time.YearMonth;
 import java.util.List;
 
@@ -7,4 +8,8 @@ public interface BudgetRepositoryCustom {
 
     boolean existsByUserIdAndBudgetYearMonthAndInCategories(String userId,
         YearMonth budgetYearMonth, List<Long> categoryIds);
+
+    List<TotalAmountByCategoryResponseDto> getTotalAmountByCategoryList(String userId);
+
+    List<TotalAmountByCategoryResponseDto> getTotalAmountByCategoryList();
 }
--- src/main/java/com/wanted/safewallet/domain/budget/persistence/repository/BudgetRepositoryImpl.java
+++ src/main/java/com/wanted/safewallet/domain/budget/persistence/repository/BudgetRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.wanted.safewallet.domain.budget.persistence.repository;
 
+import static com.querydsl.core.types.Projections.constructor;
 import static com.wanted.safewallet.domain.budget.persistence.entity.QBudget.budget;
+import static com.wanted.safewallet.domain.category.persistence.entity.QCategory.category;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.wanted.safewallet.domain.budget.persistence.dto.response.TotalAmountByCategoryResponseDto;
 import java.time.YearMonth;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,9 +30,33 @@ public class BudgetRepositoryImpl implements BudgetRepositoryCustom {
             .fetchOne());
     }
 
+    @Override
+    public List<TotalAmountByCategoryResponseDto> getTotalAmountByCategoryList(String userId) {
+        return queryFactory
+            .select(constructor(TotalAmountByCategoryResponseDto.class,
+                category, budget.amount.coalesce(0L).sum()))
+            .from(budget)
+            .rightJoin(budget.category, category).on(userIdEq(userId))
+            .groupBy(category.id)
+            .fetch();
+    }
+
+    @Override
+    public List<TotalAmountByCategoryResponseDto> getTotalAmountByCategoryList() {
+        return getTotalAmountByCategoryList(null);
+    }
+
     private BooleanExpression budgetCountCase() {
         return new CaseBuilder()
             .when(budget.count().gt(0)).then(true)
             .otherwise(false);
+    }
+
+    private BooleanExpression userIdEq(String userId) {
+        return userId == null ? alwaysTrue() : budget.user.id.eq(userId);
+    }
+
+    private BooleanExpression alwaysTrue() {
+        return Expressions.asBoolean(true).isTrue();
     }
 }
