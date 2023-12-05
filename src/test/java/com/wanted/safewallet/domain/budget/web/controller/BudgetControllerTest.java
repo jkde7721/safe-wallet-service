f9fe7f24582fef68636d00fc2cc797d11a31f508
Daeun Kim <jkde7721@naver.com>
2023-12-06 02:12:22 +0900

    test: 월별 예산 설계 컨트롤러 테스트
    [SW-14]

--- src/test/java/com/wanted/safewallet/domain/budget/web/controller/BudgetControllerTest.java
+++ src/test/java/com/wanted/safewallet/domain/budget/web/controller/BudgetControllerTest.java
@@ -1,20 +1,29 @@
 package com.wanted.safewallet.domain.budget.web.controller;
 
+import static com.wanted.safewallet.domain.category.persistence.entity.CategoryType.CLOTHING;
+import static com.wanted.safewallet.domain.category.persistence.entity.CategoryType.ETC;
+import static com.wanted.safewallet.domain.category.persistence.entity.CategoryType.FOOD;
+import static com.wanted.safewallet.domain.category.persistence.entity.CategoryType.LEISURE;
+import static com.wanted.safewallet.domain.category.persistence.entity.CategoryType.RESIDENCE;
+import static com.wanted.safewallet.domain.category.persistence.entity.CategoryType.TRAFFIC;
 import static com.wanted.safewallet.utils.JsonUtils.asJsonString;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.anyLong;
+import static org.mockito.BDDMockito.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
+import static org.mockito.BDDMockito.times;
 import static org.springframework.restdocs.payload.PayloadDocumentation.beneathPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,6 +36,8 @@ import com.wanted.safewallet.docs.common.DocsPopupLinkGenerator.DocsPopupInfo;
 import com.wanted.safewallet.domain.budget.business.service.BudgetService;
 import com.wanted.safewallet.domain.budget.web.dto.request.BudgetSetUpRequestDto;
 import com.wanted.safewallet.domain.budget.web.dto.request.BudgetUpdateRequestDto;
+import com.wanted.safewallet.domain.budget.web.dto.response.BudgetConsultResponseDto;
+import com.wanted.safewallet.domain.budget.web.dto.response.BudgetConsultResponseDto.BudgetConsultByCategoryResponseDto;
 import com.wanted.safewallet.domain.budget.web.dto.response.BudgetSetUpResponseDto;
 import com.wanted.safewallet.domain.budget.web.dto.response.BudgetSetUpResponseDto.BudgetByCategory;
 import com.wanted.safewallet.domain.budget.web.dto.response.BudgetUpdateResponseDto;
@@ -173,6 +184,57 @@ class BudgetControllerTest extends AbstractRestDocsTest {
             .andExpect(jsonPath("$.message", AllOf.allOf(
                 containsString("budgetYearMonth: 월별 예산 수정값이 널일 수 없습니다."),
                 containsString("amount: 월별 예산 금액은 0원 이상이어야 합니다."))))
+            .andDo(print());
+    }
+
+    @DisplayName("월별 예산 설계 컨트롤러 테스트 : 성공")
+    @Test
+    void consultBudget() throws Exception {
+        //given
+        Long totalAmount = 1_000_000L;
+        List<BudgetConsultByCategoryResponseDto> budgetConsultList = List.of(
+            new BudgetConsultByCategoryResponseDto(1L, FOOD, 200_000L),
+            new BudgetConsultByCategoryResponseDto(2L, TRAFFIC, 150_000L),
+            new BudgetConsultByCategoryResponseDto(3L, RESIDENCE, 500_000L),
+            new BudgetConsultByCategoryResponseDto(4L, CLOTHING, 100_000L),
+            new BudgetConsultByCategoryResponseDto(5L, LEISURE, 30_000L),
+            new BudgetConsultByCategoryResponseDto(6L, ETC, 20_000L));
+        BudgetConsultResponseDto responseDto = new BudgetConsultResponseDto(budgetConsultList);
+        given(budgetService.consultBudget(anyString(), anyLong())).willReturn(responseDto);
+
+        //when, then
+        restDocsMockMvc.perform(get("/api/budgets/consult")
+                .param("totalAmount", String.valueOf(totalAmount))
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.budgetConsultList", hasSize(6)))
+            .andDo(restDocs.document(
+                queryParameters(
+                    parameterWithName("totalAmount").description("예산 설계 총액")
+                        .attributes(key("constraints").value("0원 이상 100,000,000원 이하"))
+                ),
+                responseFields(
+                    beneathPath("data").withSubsectionId("data"),
+                    fieldWithPath("budgetConsultList").description("예산 설계 결과 리스트"),
+                    fieldWithPath("budgetConsultList[].categoryId").description("카테고리 id"),
+                    fieldWithPath("budgetConsultList[].type").description(DocsPopupLinkGenerator
+                        .generatePopupLink(DocsPopupInfo.CATEGORY_TYPE)),
+                    fieldWithPath("budgetConsultList[].amount").description("카테고리 별 예산 금액"))
+            ));
+    }
+
+    @DisplayName("월별 예산 설계 컨트롤러 테스트 : 실패")
+    @Test
+    void consultBudget_validation_fail() throws Exception {
+        //given
+        Long totalAmount = 1000_000_000L;
+
+        //when, then
+        mockMvc.perform(get("/api/budgets/consult")
+                .param("totalAmount", String.valueOf(totalAmount))
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message", containsString("월별 예산 설계를 위한 예산 총액은 100000000원 이하이어야 합니다.")))
             .andDo(print());
     }
 }
