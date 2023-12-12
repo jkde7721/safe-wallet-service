cf57a8b148c33420dbe98f0c71e7aaa2e54361d6
Daeun Kim <jkde7721@naver.com>
2023-12-12 17:40:51 +0900

    feat: 오늘 지출 안내 서비스에 캐싱 적용
    - 오늘 지출 추천 서비스에서 캐싱한 데이터 조회
    
    [SW-24]

--- src/main/java/com/wanted/safewallet/domain/expenditure/business/service/ExpenditureDailyStatsService.java
+++ src/main/java/com/wanted/safewallet/domain/expenditure/business/service/ExpenditureDailyStatsService.java
@@ -3,14 +3,14 @@ package com.wanted.safewallet.domain.expenditure.business.service;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
 
-import com.wanted.safewallet.domain.budget.business.service.BudgetService;
 import com.wanted.safewallet.domain.category.persistence.entity.Category;
 import com.wanted.safewallet.domain.expenditure.business.mapper.ExpenditureMapper;
 import com.wanted.safewallet.domain.expenditure.business.vo.TodayExpenditureDailyStatsVo;
 import com.wanted.safewallet.domain.expenditure.persistence.repository.ExpenditureRepository;
+import com.wanted.safewallet.domain.expenditure.web.dto.response.TodayExpenditureConsultResponseDto;
+import com.wanted.safewallet.domain.expenditure.web.dto.response.TodayExpenditureConsultResponseDto.TodayExpenditureConsultOfCategoryResponseDto;
 import com.wanted.safewallet.domain.expenditure.web.dto.response.TodayExpenditureDailyStatsResponseDto;
 import java.time.LocalDate;
-import java.time.YearMonth;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,17 +22,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class ExpenditureDailyStatsService {
 
     private final ExpenditureMapper expenditureMapper;
-    private final BudgetService budgetService;
+    private final ExpenditureConsultService expenditureConsultService;
     private final ExpenditureRepository expenditureRepository;
 
     public TodayExpenditureDailyStatsResponseDto produceTodayExpenditureDailyStats(String userId) {
-        LocalDate expenditureDate = LocalDate.now();
-        YearMonth currentYearMonth = YearMonth.of(expenditureDate.getYear(), expenditureDate.getMonth());
-        int daysOfCurrentMonth = currentYearMonth.lengthOfMonth();
-        Map<Category, Long> budgetTotalAmountByCategory = budgetService.getBudgetTotalAmountByCategory(userId, currentYearMonth);
-        //TODO: 아래 값 정확하지 않음 (현재까지의 지출 고려X)
-        Map<Category, Long> budgetAmountPerDayByCategory = getBudgetAmountPerDayByCategory(budgetTotalAmountByCategory, daysOfCurrentMonth);
-        Map<Category, Long> todayExpenditureTotalAmountByCategory = expenditureRepository.findTotalAmountMapByUserAndExpenditureDate(userId, expenditureDate);
+        LocalDate now = LocalDate.now();
+        TodayExpenditureConsultResponseDto todayExpenditureConsult = expenditureConsultService.consultTodayExpenditure(userId);
+        Map<Category, Long> budgetAmountPerDayByCategory = convertToBudgetAmountPerDayByCategory(todayExpenditureConsult);
+        Map<Category, Long> todayExpenditureTotalAmountByCategory = expenditureRepository.findTotalAmountMapByUserAndExpenditureDate(userId, now);
 
         Long todayTotalAmount = calculateTodayTotalAmount(todayExpenditureTotalAmountByCategory);
         Map<Category, TodayExpenditureDailyStatsVo> todayExpenditureDailyStatsByCategory =
@@ -40,15 +37,10 @@ public class ExpenditureDailyStatsService {
         return expenditureMapper.toDto(todayTotalAmount, todayExpenditureDailyStatsByCategory);
     }
 
-    //추후 캐시 적용으로 제거할 메소드
-    private Map<Category, Long> getBudgetAmountPerDayByCategory(Map<Category, Long> budgetTotalAmountByCategory, int daysOfCurrentMonth) {
-        return budgetTotalAmountByCategory.keySet().stream().collect(toMap(identity(), category ->
-            calculateAmountPerDay(budgetTotalAmountByCategory.get(category), daysOfCurrentMonth)));
-    }
-
-    //추후 캐시 적용으로 제거할 메소드
-    private Long calculateAmountPerDay(Long totalAmount, int days) {
-        return (long) ((double) totalAmount / days / 100) * 100; //100원 단위로 환산
+    private Map<Category, Long> convertToBudgetAmountPerDayByCategory(TodayExpenditureConsultResponseDto todayExpenditureConsult) {
+        return todayExpenditureConsult.getTodayExpenditureConsultOfCategoryList().stream()
+            .collect(toMap(consult -> Category.builder().id(consult.getCategoryId()).type(consult.getType()).build(),
+                TodayExpenditureConsultOfCategoryResponseDto::getTodayTotalAmount));
     }
 
     private Long calculateTodayTotalAmount(Map<Category, Long> todayExpenditureTotalAmountByCategory) {
