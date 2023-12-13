0caa30c4f0cc7cbb285743093c8fc43a39c72600
Daeun Kim <jkde7721@naver.com>
2023-12-13 18:00:52 +0900

    update: ExpenditureDetailsResponseDto 내 expenditureDate JsonFormat pattern 추가
    - LocalDateTime 타입으로의 변경으로 직렬화 패턴 지정
    
    [SW-26]

--- src/main/java/com/wanted/safewallet/domain/expenditure/web/dto/response/ExpenditureDetailsResponseDto.java
+++ src/main/java/com/wanted/safewallet/domain/expenditure/web/dto/response/ExpenditureDetailsResponseDto.java
@@ -1,5 +1,8 @@
 package com.wanted.safewallet.domain.expenditure.web.dto.response;
 
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wanted.safewallet.domain.category.persistence.entity.CategoryType;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -11,6 +14,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ExpenditureDetailsResponseDto {
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = STRING, timezone = "Asia/Seoul")
     private LocalDateTime expenditureDate;
 
     private Long amount;
