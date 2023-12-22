8ce3639cd5f53c5adf93582873ac66ac681f9c4d
Daeun Kim <jkde7721@naver.com>
2023-12-22 19:26:57 +0900

    feat: 계정 비활성화한 유저가 재로그인 시 계정 복원되도록 구현
    - 로그인 시 동작하는 CustomUserDetailsService 내에서 UserService 의존하여 User 엔티티 조회
    - 이때 활성화 계정 조회 -> 비활성화 계정 조회 -> 예외 발생 순으로 동작, 비활성화 계정 조회 시 계정 복원
    
    [SW-31]

--- src/main/java/com/wanted/safewallet/domain/auth/business/service/CustomUserDetailsService.java
+++ src/main/java/com/wanted/safewallet/domain/auth/business/service/CustomUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.wanted.safewallet.domain.auth.business.service;
 
 import com.wanted.safewallet.domain.auth.business.dto.CustomUserDetails;
+import com.wanted.safewallet.domain.user.business.service.UserService;
 import com.wanted.safewallet.domain.user.persistence.entity.User;
-import com.wanted.safewallet.domain.user.persistence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,12 +13,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-            .orElseThrow(() -> new UsernameNotFoundException("잘못된 계정명입니다."));
+        //활성화 계정 조회 -> 비활성화 계정 조회 -> 예외 발생
+        User user = userService.getActiveUserByUsername(username)
+            .orElseGet(() -> userService.getRestoredUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("잘못된 계정명입니다.")));
         return new CustomUserDetails(user);
     }
 }
