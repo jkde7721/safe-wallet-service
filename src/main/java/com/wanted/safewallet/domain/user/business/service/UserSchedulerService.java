a0295de8c1b3617de97a9eab4f602db149e1e4d1
Daeun Kim <jkde7721@naver.com>
2023-12-23 13:02:50 +0900

    feat: 3개월 이상의 비활성 계정 탈퇴 로직 스케줄러 구현
    - 매주 일요일 23시마다 비활성 계정 중 3개월 내에 재로그인하지 않은 유저 관련 데이터 모두 삭제
    
    [SW-31]

---
+++ src/main/java/com/wanted/safewallet/domain/user/business/service/UserSchedulerService.java
@@ -0,0 +1,28 @@
+package com.wanted.safewallet.domain.user.business.service;
+
+import com.wanted.safewallet.domain.budget.business.service.BudgetService;
+import com.wanted.safewallet.domain.expenditure.business.service.ExpenditureService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class UserSchedulerService {
+
+    private final UserService userService;
+    private final BudgetService budgetService;
+    private final ExpenditureService expenditureService;
+
+    @Scheduled(cron = "0 0 23 ? * SUN *") //매주 일요일 23시마다
+    @Transactional
+    public void withdrawUsers() {
+        List<String> userIds = userService.getWithdrawnUserIds();
+        budgetService.deleteByUserIds(userIds);
+        expenditureService.deleteByUserIds(userIds);
+        userService.withdrawByIds(userIds);
+    }
+}
