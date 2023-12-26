642963cf51ebf5c3cf16714ab1386dccdec05e71
Daeun Kim <jkde7721@naver.com>
2023-12-26 14:41:23 +0900

    feat: 회원가입 시 메일 인증 로직 구현
    - 회원가입 시 유저의 이메일로 메일 전송하여 메일 인증 진행
    - 기존 메일 인증 실패했을 경우 인증용 메일 재발송 가능
    
    [SW-33]

--- src/main/java/com/wanted/safewallet/domain/user/business/facade/UserFacadeService.java
+++ src/main/java/com/wanted/safewallet/domain/user/business/facade/UserFacadeService.java
@@ -1,10 +1,19 @@
 package com.wanted.safewallet.domain.user.business.facade;
 
+import static java.util.UUID.randomUUID;
+
 import com.wanted.safewallet.domain.user.business.mapper.UserMapper;
+import com.wanted.safewallet.domain.user.business.service.UserMailCodeService;
 import com.wanted.safewallet.domain.user.business.service.UserService;
 import com.wanted.safewallet.domain.user.persistence.entity.User;
 import com.wanted.safewallet.domain.user.web.dto.request.UserJoinRequest;
+import com.wanted.safewallet.domain.user.web.dto.request.UserMailRequest;
 import com.wanted.safewallet.domain.user.web.dto.response.UsernameCheckResponse;
+import com.wanted.safewallet.global.exception.BusinessException;
+import com.wanted.safewallet.global.exception.BusinessTemplateException;
+import com.wanted.safewallet.global.mail.dto.MailMessage;
+import com.wanted.safewallet.global.mail.service.MailService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,7 +26,14 @@ public class UserFacadeService {
 
     private final UserMapper userMapper;
     private final UserService userService;
+    private final UserMailCodeService userMailCodeService;
+    private final MailService mailService;
     private final PasswordEncoder passwordEncoder;
+
+    private static final String MAIL_AUTH_SUBJECT = "[Safe Wallet] 회원가입을 위해 메일을 인증해 주세요.";
+    private static final String MAIL_AUTH_CODE_NAME = "code";
+    private static final String MAIL_AUTH_EMAIL_NAME = "email";
+    private static final String MAIL_AUTH_TEMPLATE = "mail-auth";
 
     public UsernameCheckResponse checkForUsername(String username) {
         boolean isDuplicatedUsername = userService.isDuplicatedUsername(username);
@@ -30,11 +46,42 @@ public class UserFacadeService {
         String encodedPassword = passwordEncoder.encode(request.getPassword());
         User user = userMapper.toEntity(request, encodedPassword);
         userService.saveUser(user);
+        processMailAuthMessage(user.getUsername());
     }
 
     @Transactional
     public void deactivateUser(String userId) {
         User user = userService.getUser(userId);
         userService.deleteUser(user);
+    }
+
+    @Transactional
+    public void authenticateMail(String email, String code) {
+        try {
+            userMailCodeService.validateMailCode(email, code);
+            userService.upgradeToUserRole(email);
+        } catch (BusinessException e) {
+            throw new BusinessTemplateException(
+                e, Map.of("userMailRequest", new UserMailRequest(email)), "mail-auth-fail");
+        }
+    }
+
+    public void resendMailAuth(String email) {
+        try {
+            userService.checkForExistingMailAuth(email);
+            processMailAuthMessage(email);
+        } catch (BusinessException e) {
+            throw new BusinessTemplateException(e, "mail-auth-resend-fail");
+        }
+    }
+
+    private void processMailAuthMessage(String toEmail) {
+        String code = randomUUID().toString();
+        MailMessage mailAuthMessage = MailMessage.builder()
+            .to(toEmail).subject(MAIL_AUTH_SUBJECT)
+            .variables(Map.of(MAIL_AUTH_EMAIL_NAME, toEmail, MAIL_AUTH_CODE_NAME, code))
+            .template(MAIL_AUTH_TEMPLATE).build();
+        userMailCodeService.saveMailCode(toEmail, code);
+        mailService.sendMail(mailAuthMessage); //비동기
     }
 }
--- src/main/java/com/wanted/safewallet/domain/user/web/dto/request/UserJoinRequest.java
+++ src/main/java/com/wanted/safewallet/domain/user/web/dto/request/UserJoinRequest.java
@@ -1,6 +1,8 @@
 package com.wanted.safewallet.domain.user.web.dto.request;
 
+import com.wanted.safewallet.domain.user.web.validation.ValidEmail;
 import com.wanted.safewallet.domain.user.web.validation.ValidPassword;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,7 +14,9 @@ import lombok.NoArgsConstructor;
 @ValidPassword
 public class UserJoinRequest {
 
-    @NotBlank(message = "{user.join.username}")
+    @ValidEmail
+    @Email(message = "{user.username.email}")
+    @NotBlank(message = "{user.username.notBlank}")
     private String username;
 
     private String password;
