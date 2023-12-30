64dc38c38ed8aeebeffe356a4eac41e30306b2a7
Daeun Kim <jkde7721@naver.com>
2023-12-30 23:08:53 +0900

    update: 오 되네ㅎ
    [SW-29]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 BUILD_JAR=$(ls /home/ec2-user/app/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build : $JAR_NAME" >> /home/ec2-user/deploy.log
+DEPLOY_LOG=/home/ec2-user/deploy.log
+DEPLOY_ERR_LOG=/home/ec2-user/deploy_err.log
 
-echo "> build 파일 복사" >> /home/ec2-user/deploy.log
+echo "> build 파일 복사" >> DEPLOY_LOG
 DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
+echo "> 실행 중인 애플리케이션 pid 확인" >> DEPLOY_LOG
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 실행중인 애플리케이션이 없으므로 종료하지 않음" >> /home/ec2-user/deploy.log
+  echo "> 실행 중인 애플리케이션이 없으므로 종료하지 않음" >> DEPLOY_LOG
 else
-  echo "> kill -15 $CURRENT_PID" >> /home/ec2-user/deploy.log
+  echo "> kill -15 $CURRENT_PID" >> DEPLOY_LOG
   kill -15 $CURRENT_PID
   sleep 10
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo $DATABASE_URL >> /home/ec2-user/deploy.log
-echo $REDIS_URL >> /home/ec2-user/deploy.log
+echo "> 환경 변수 설정" >> DEPLOY_LOG
 source ~/.bashrc
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> 새로운 애플리케이션 배포" >> DEPLOY_LOG
+nohup java -jar $DEPLOY_JAR >> DEPLOY_LOG 2>DEPLOY_ERR_LOG &
